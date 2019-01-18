e22cec7cd6a444d35248966a482e2e3767c54793
Thomas <thomas.dewit@outlook.fr>
2019-01-18 10:26:39 +0100

    Import

---
+++ store_temp.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$data_json = file_get_contents("php://input");
+$filename = "data.json";
+
+//vérification du json
+$data = json_decode($data_json);
+if (! $data) {
+	http_response_code(415);
+	exit();
+}elseif (! $data->temperature || ! $data->humidite) {
+	http_response_code(400);
+	exit();
+}
+
+//écriture des données
+$op = file_put_contents($filename, $data_json) ;
+
+//vérification de l'écriture des données
+if (! $op) {
+	http_response_code(500);
+	exit();
+} ?>
